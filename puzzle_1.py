9b409bdd609f43c36d04aa80afff6a2cd54b914f
Ariel Pillemer <arip@MacBook-Pro.local>
2021-12-01 14:53:52 +0000

    puzzle 1 + 2

---
+++ puzzle_1.py
@@ -0,0 +1,20 @@
+# advent of code 2021
+# Puzzle 1
+# How many measurements are larger than the previous measurement?
+from puzzle_1_input import raw_input 
+
+input = [int(entry) for entry in raw_input.split()]
+
+def counter(list_of_depths):
+    deeper_count = 0
+    shallower_count = 0
+    prev_depth = list_of_depths[0]
+    for depth in list_of_depths:
+        if depth > prev_depth:
+            deeper_count += 1
+        elif depth < prev_depth:
+            shallower_count += 1
+        prev_depth = depth
+        
+    print(f'Deeper: {deeper_count}')
+    print(f'Shallower: {shallower_count}')
---
+++ puzzle_2.py
@@ -0,0 +1,13 @@
+# advent of code 2021
+# Puzzle 2
+# count the number of times the sum of measurements in this sliding window increases from the previous sum.
+from puzzle_1_input import raw_input 
+from puzzle_1 import counter
+
+input = [int(entry) for entry in raw_input.split()]
+
+windows = []
+for i in range(2, len(input)):
+    windows.append(input[i] + input[i-1] + input[i-2])
+
+counter(windows)
